2899b6ac256383b55dae0373283259b2eda81155
Dcode9 <194372792+Dcode9@users.noreply.github.com>
2026-05-29 08:24:32 +0000

    Optimize for mobile
    - Fix blank board creation
    - Move search bar to bottom on mobile
    - Make top bar UI minimal and hide text
    - Remove "+ New Board" button
    - Put local/cloud boards above templates
    - Hide empty local/cloud board sections
    - Make templates minimized by default if boards exist
    - Replace template toggle text with an arrow icon

--- patch.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-
-// 1. Remove .header-account-container from #whiteboard-view
-// Actually, I can just replace lines where we have:
-// <div class="floating-container header-account-container" style="display: none;">
-// ...
-// </div>
